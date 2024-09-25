3958ee4ab3cfc3ddbb3a6c54a95d0b88b32d2ead
anthonyozerov <anthony@ozerov.email>
2024-09-25 15:48:11 -0700

    make cnn job multicpu

--- experiments/cnn/job.sh
+++ experiments/cnn/job.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -l
+#!/bin/bash
 
 # take config filename from first argument
-config=$1
 
 #SBATCH --job-name=cnn
 #SBATCH --partition=jsteinhardt
 #SBATCH --gres=gpu:A5000:1
+#SBATCH --cpus-per-task=4
 
-python ../../run_cnn.py configs/$config
+python ../../run_cnn.py configs/$1.yaml
