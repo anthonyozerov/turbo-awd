d33a1a072d22071058ba5c0ad17755354e5782e3
anthonyozerov <anthony@ozerov.email>
2024-10-20 22:19:16 -0700

    rename online cnn exp, add gm4 baseline

---
+++ experiments/online/gm4_baseline.py
@@ -0,0 +1,24 @@
+from py2d.Py2D_solver import Py2D_solver
+import sys
+sys.path.insert(0, 'turboawd')
+print(sys.path)
+import os
+print(os.path.abspath(sys.path[0]))
+
+Py2D_solver(Re=20e3, # Reynolds number
+               fkx=25, # Forcing wavenumber in x
+               fky=25, # Forcing wavenumber in y
+               alpha=0.1, # Rayleigh drag coefficient
+               beta=0, # Coriolis parameter
+               NX=128, # Number of grid points in x and y '32', '64', '128', '256', '512'
+               SGSModel_string='PiOmegaGM4', # SGS model to use 'NoSGS', 'SMAG', 'DSMAG', 'LEITH', 'DLEITH', 'PiOmegaGM2', 'PiOmegaGM4', 'PiOmegaGM6'
+               eddyViscosityCoeff=0, # Coefficient for eddy viscosity models: SMAG and LEITH
+               dt=1e-4, # Time step
+               saveData=True, # Save data
+               dealias=False, # dealias
+               tSAVE=1.0, # Time interval to save data
+               tTotal=1, # Total time of simulation
+               readTrue=False,
+               ICnum='fdns_ic_0_train', # Initial condition number: Choose between 1 to 20
+               resumeSim=False, # tart new simulation (False) or resume simulation (True)
+               )
