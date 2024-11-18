cd9d6bd497c52cd220cccd587ceecbaafdd1ab80
anthonyozerov <anthony@ozerov.email>
2024-11-18 04:24:21 -0800

    add online-cnn job

---
+++ experiments/online/job-cnn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# take config filename from first and second arguments
+# USAGE: sbatch job-cnn.sh config cnn_config
+
+#SBATCH --job-name=online-cnn
+#SBATCH --partition=jsteinhardt
+#SBATCH --gres=gpu:A5000:1
+#SBATCH --cpus-per-task=8
+
+python run_online.py $1 $2
