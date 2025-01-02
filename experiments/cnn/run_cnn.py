49ea43f89d15bed3d35efb70c7e4b8b916368853
anthonyozerov <anthony@ozerov.email>
2025-01-01 18:08:46 -0800

    refactor config and data loading

--- experiments/cnn/run_cnn.py
+++ experiments/cnn/run_cnn.py
@@ -19,33 +19,16 @@ from awave2.ddw.loss import DDWLossConfig
 
 from turboawd.cnn import CNN
 from turboawd.net import Net
+from turboawd.utils import load_cnn_config, load_data, normalize
 
 ########################## LOAD CONFIGS ##############################
 print('Loading configs')
 # load configuration
 # the main config file specifies the names of the other config files
 config_path = sys.argv[1]
-assert os.path.exists(config_path), f"Invalid config path: {config_path}"
-config_meta = yaml.safe_load(open(config_path, "r"))
 
-# get the names of the other config files
-architecture = config_meta["architecture"]
-channels = config_meta["channels"]
-data = config_meta["data"]
-training = config_meta["training"]
+config, name = load_cnn_config(config_path)
 
-# load the other config files
-config_architecture = yaml.safe_load(
-    open(f"configs/architecture/{architecture}.yaml", "r")
-)
-config_channels = yaml.safe_load(open(f"configs/channels/{channels}.yaml", "r"))
-config_data = yaml.safe_load(open(f"configs/data/{data}.yaml", "r"))
-config_training = yaml.safe_load(open(f"configs/training/{training}.yaml", "r"))
-
-# combine them all into one config
-config = {**config_architecture, **config_channels, **config_data, **config_training}
-
-name = config_path.split("/")[-1].split(".")[0]
 print(name)
 
 # if slurm, save job id to config
@@ -65,84 +48,33 @@ train_input_path = f"{train_dir}/{train_input_file}"
 # keys says which channels will be used
 keys = config["input_channels"]
 n_channels = len(keys)
-
-# load input data
-with h5py.File(train_input_path, "r") as f:
-    data = [
-        np.array(f[key], np.float32) for key in keys
-    ]
-
-# normalize the input data by the mean and standard deviation
-# of each channel
-data = [(d - np.mean(d)) / np.std(d) for d in data]
-
-# calculate shape of input data
-Nlon = data[0].shape[0]
-Nlat = data[0].shape[1]
-N = data[0].shape[2]
-
-data = np.array(data)
-# data is [channels, Nlon, Nlat, N]
-data = np.moveaxis(data, -1, 0)
-# data is [N, channels, Nlon, Nlat]
-assert data.shape == (N, n_channels, Nlon, Nlat)
-
 train_N = config["data"]["train_N"]
 
-train_input_mat = data[:train_N, :, :, :]
-val_input_mat = data[train_N:, :, :, :]
+input_train = load_data(train_input_path, keys, centerscale=True, before=train_N, tensor=True)
+input_val = load_data(train_input_path, keys, centerscale=True, after=train_N, tensor=True)
+assert input_train.shape == (train_N, n_channels, 128, 128)
 
-assert train_input_mat.shape == (train_N, n_channels, Nlon, Nlat)
-assert val_input_mat.shape == (N - train_N, n_channels, Nlon, Nlat)
-
-# load output data
-train_output_file = config["data"]["train_output_file"]
-train_output_path = f"{train_dir}/{train_output_file}"
-with h5py.File(train_output_path, "r") as f:
-    data = np.array(f["PI"], np.float32)
-
-# make output data be residual if needed
+train_output_path = config["data"]["train_dir"] + "/" + config["data"]["train_output_file"]
 if "residual" in config:
-    from scipy.io import loadmat
+    norm_path = config["data"]["train_dir"] + "/" + config["data"]["norm_file"]
+    output = load_data(train_output_path, ["PI"], norm_path=norm_path,
+                       norm_keys=["IPI"], denorm=True, centerscale=False)
+    residual = load_data(train_input_path, [config["residual"]])
+    output -= residual
+    output = normalize(output, norm_path, ["IPI"], sd_only=True)
+else:
+    output = load_data(train_output_path, ["PI"])
 
-    if "norm_file" in config["data"]:
-        norm_path = config["data"]["train_dir"] + "/" + config["data"]["norm_file"]
-    else:
-        norm_path = (
-            config["data"]["train_dir"] + "/Normalization_coefficients_train.mat"
-        )
-    normalization = loadmat(norm_path)
+output_train = output[:train_N]
+output_val = output[train_N:]
 
-    # unnormalize the PI data
-    data = data * normalization["SDEV_IPI"][0][0] + normalization["MEAN_IPI"][0][0]
+assert output_train.shape == (train_N, 1, 128, 128)
 
-    # take the residual of the PI data with another channel (e.g. output of GM4)
-    with h5py.File(train_input_path, "r") as f:
-        data -= np.array(f[config["residual"]], np.float32)
-    # renormalize the residual
-    data = data / normalization["SDEV_IPI"][0][0]
+output_train = torch.from_numpy(output_train).float()
+output_val = torch.from_numpy(output_val).float()
 
-# output data is [Nlon, Nlat, N]
-data = np.moveaxis(data, -1, 0)
-data = np.expand_dims(data, axis=1)
-assert data.shape == (N, 1, Nlon, Nlat)
-
-train_output_mat = data[:train_N, :, :, :]
-val_output_mat = data[train_N:, :, :, :]
-
-assert train_output_mat.shape == (train_N, 1, Nlon, Nlat)
-assert val_output_mat.shape == (N - train_N, 1, Nlon, Nlat)
-
-# make inputs and outputs TensorDataset
-
-train_input_mat = torch.from_numpy(train_input_mat).float()
-train_output_mat = torch.from_numpy(train_output_mat).float()
-
-val_input_mat = torch.from_numpy(val_input_mat).float()
-val_output_mat = torch.from_numpy(val_output_mat).float()
-
-trainset = TensorDataset(train_input_mat, train_output_mat)
-valset = TensorDataset(val_input_mat, val_output_mat)
+trainset = TensorDataset(input_train, output_train)
+valset = TensorDataset(input_val, output_val)
 
 # define dataloaders
 trainloader = DataLoader(trainset, **config["dataloader_train"])
---
+++ turboawd/utils.py
@@ -0,0 +1,169 @@
+import os
+import yaml
+import h5py
+import numpy as np
+from scipy.io import loadmat
+
+
+def load_cnn_config(config_path):
+    """
+    Load the configuration files for a CNN experiment.
+
+    Parameters
+    ----------
+    config_path : str
+        Path to the main configuration file.
+
+    Returns
+    -------
+    config : dict
+        Dictionary containing all configuration options.
+    name : str
+        Name of the CNN.
+    """
+
+    assert os.path.exists(config_path), f"Invalid config path: {config_path}"
+    config_meta = yaml.safe_load(open(config_path, "r"))
+
+    # get the names of the other config files
+    architecture = config_meta["architecture"]
+    channels = config_meta["channels"]
+    data = config_meta["data"]
+    training = config_meta["training"]
+
+    # load the other config files
+    config_architecture = yaml.safe_load(
+        open(f"configs/architecture/{architecture}.yaml", "r")
+    )
+    config_channels = yaml.safe_load(open(f"configs/channels/{channels}.yaml", "r"))
+    config_data = yaml.safe_load(open(f"configs/data/{data}.yaml", "r"))
+    config_training = yaml.safe_load(open(f"configs/training/{training}.yaml", "r"))
+
+    # combine them all into one config
+    config = {
+        **config_architecture,
+        **config_channels,
+        **config_data,
+        **config_training,
+    }
+
+    name = config_path.split("/")[-1].split(".")[0]
+
+    return config, name
+
+
+def normalize(data, norm_path, norm_keys, sd_only=False):
+    assert os.path.exists(norm_path), f"Invalid normalization path: {norm_path}"
+    normalization = loadmat(norm_path)
+    for i in range(len(data)):
+        mean = normalization["MEAN_" + norm_keys[i]][0][0]
+        sdev = normalization["SDEV_" + norm_keys[i]][0][0]
+        if sd_only:
+            data[i] = data[i] / sdev
+        else:
+            data[i] = (data[i] - mean) / sdev
+    return data
+
+
+def denormalize(data, norm_path, norm_keys):
+    assert os.path.exists(norm_path), f"Invalid normalization path: {norm_path}"
+    normalization = loadmat(norm_path)
+    for i in range(len(data)):
+        mean = normalization["MEAN_" + norm_keys[i]][0][0]
+        sdev = normalization["SDEV_" + norm_keys[i]][0][0]
+        data[i] = data[i] * sdev + mean
+    return data
+
+
+def load_data(
+    path,
+    keys,
+    centerscale=False,
+    norm_path=None,
+    norm_keys=None,
+    denorm=False,
+    before=None,
+    after=None,
+    tensor=False,
+):
+    """
+    Load fluid data from a .h5 file, optionally normalizing/denormalizing it. The .h5
+    file has keys for channels, and every channel has shape [Nlon, Nlat, N].
+    Data is returned in the shape [N, channels, Nlon, Nlat], suitable for use in PyTorch
+    or ONNX.
+
+    Parameters
+    ----------
+    path : str
+        Path to the .h5 file.
+    keys : list of str
+        List of keys to load from the .h5 file.
+    centerscale : bool, optional
+        Whether to center and scale the data, by default False. Mutually exclusive with normalizing
+        using a normalization file.
+    norm_path : str, optional
+        Path to the normalization file, by default None. If norm_path is specified and
+        denorm is False, the data will be normalized using the given mean and std for each channel
+        in the normalization file. If denorm is True, the data will be denormalized.
+    norm_keys : list of str, optional
+        List of keys to normalize, by default None. Must be specified if norm_path is not None.
+        Keys should be in the same order as the keys parameter.
+    denorm : bool, optional
+        Whether to denormalize the data, by default False.
+    before : int, optional
+        Only keep samples up to this index, by default None.
+    after : int, optional
+        Only keep samples after this index, by default None.
+    tensor : bool, optional
+        Whether to convert the data to a PyTorch tensor, by default False.
+
+    Returns
+    -------
+    np.ndarray or torch.Tensor
+        Data loaded from the .h5 file.
+    """
+
+    if norm_path is not None:
+        assert norm_keys is not None
+        assert len(norm_keys) == len(keys)
+    assert before is None or after is None
+    assert norm_path is None or centerscale is False
+
+    assert os.path.exists(path), f"Invalid data path: {path}"
+
+    with h5py.File(path, "r") as f:
+        data = [np.array(f[key], np.float32) for key in keys]
+    data = np.array(data)
+    # data is [channels, Nlon, Nlat, N]
+    data = np.moveaxis(data, -1, 0)
+    # data is [N, channels, Nlon, Nlat]
+
+    if centerscale:
+        # center and scale each channel
+        means = np.mean(data, axis=(0, 2, 3), keepdims=True)
+
+        data = (data - np.mean(data, axis=(0, 2, 3), keepdims=True)) / np.std(
+            data, axis=(0, 2, 3), keepdims=True
+        )
+
+        # check that each channel has mean 0 and std 1
+        assert np.allclose(np.mean(data, axis=(0, 2, 3)), 0, atol=1e-6)
+        assert np.allclose(np.std(data, axis=(0, 2, 3)), 1, atol=1e-2)
+
+    if before is not None:
+        data = data[:before, :, :, :]
+    if after is not None:
+        data = data[after:, :, :, :]
+
+    if norm_path is not None:
+        if denorm:
+            data = denormalize(data, norm_path, norm_keys)
+        else:
+            data = normalize(data, norm_path, norm_keys)
+
+    if tensor:
+        import torch
+
+        data = torch.from_numpy(data)
+
+    return data
